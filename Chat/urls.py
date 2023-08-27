779bf520bc80f58e7700f4f0f01fe49225bf84db
alireza <ansari09165630639@gmail.com>
2023-08-27 21:23:42 +0330

    add chat room

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from .views import index
+from .views import *
 urlpatterns = [
-    path('',index)
+    path('',index,name='index'),
+    path('<str:room_name>/',room,name='room'),
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index (request):
     return render(request,'Chat/index.html')
+
+def room(request,room_name):
+    context={
+        'room_name': room_name
+    }
+    return render(request,'Chat/room.html',context)
