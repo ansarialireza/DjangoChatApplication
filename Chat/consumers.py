3660b01469ecc0bfb4ce13a74e0772855eef5d8b
alireza <ansari09165630639@gmail.com>
2023-09-05 17:47:13 +0330

    add css , and login ,...

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,66 +1,106 @@
-# chat/consumers.py
 from channels.generic.websocket import WebsocketConsumer
 import json
-
 from asgiref.sync import async_to_sync
-from .serializers import MessageSerializers
-from .models import Message
+from .serializers import MessageSerializer
+from .models import Message, Chat
 from rest_framework.renderers import JSONRenderer
+from django.contrib.auth import get_user_model
+
+
+user = get_user_model()
+
+
 class ChatConsumer(WebsocketConsumer):
-
-    def new_message(self,data):
-        ...
-    def fetch_message(self,data):
-        qs=Message.last_message(self)
-        message_json=self.message_serializer(qs)
-        content={
-            'message':eval(message_json)
+    
+    def new_message(self, data):
+        message = data['message']
+        author = data['username']
+        roomname = data['roomname']
+        chat_model = Chat.objects.get(roomname=roomname)
+        user_model = user.objects.filter(username=author).first()
+        message_model = Message.objects.create(author=user_model , content=message, related_chat=chat_model)
+        result = eval(self.message_serializer(message_model))
+        self.send_to_chat_message(result)
+    
+    
+    
+    def fetch_message(self, data):
+        roomname = data['roomname']
+        qs = Message.last_message(self, roomname)
+        message_json = self.message_serializer(qs)
+        content = {
+            
+            "message" : eval(message_json),
+            'command' : "fetch_message"
+            
         }
-
-    def message_serializer(self,qs):
-        serialized=MessageSerializers(qs,many=True)
-        content=JSONRenderer().render(serialized.data)
+        self.chat_message(content)
+    
+    def message_serializer(self, qs):
+    
+        serialized = MessageSerializer(qs, many=(lambda qs : True if (qs.__class__.__name__ == 'QuerySet') else False)(qs))
+        content = JSONRenderer().render(serialized.data)
         return content
-
+        
+    
+    
+    
+    
+    
+    
+    
     def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-
-        # Join room group
+        
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
+            self.room_group_name,
+            self.channel_name
+        )    
         self.accept()
 
-
-    commands={
-        "new_message":new_message,
-        "fetch_message":fetch_message
+    commands = {
+        
+        "new_message": new_message,
+        "fetch_message": fetch_message
+    
     }
-
-
+    
     def disconnect(self, close_code):
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
 
-    # Receive message from WebSocket
+            self.room_group_name,
+            self.channel_name
+            
+        )
+    
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json.get('message',None)
-        command=text_data_json['command']
-        self.commands[command](self,message)
-
-        # Send message to room group
-    def send_to_chat_message(self,message):            
+        text_data_dict = json.loads(text_data)
+        
+        command = text_data_dict['command']
+        
+        self.commands[command](self, text_data_dict)
+        
+        
+        
+    def send_to_chat_message(self, message):    
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'content': message['content'],
+                'command':'new_message',
+                '__str__' : message['__str__']
+                
+                
+                
+            }
+            
+            
         )
 
-    # Receive message from room group
+    
     def chat_message(self, event):
-        message = event["message"]  
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+       
+        self.send(text_data=json.dumps(event))
--- Chat/views.py
+++ Chat/views.py
@@ -1,12 +1,50 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+from django.contrib.auth.decorators import login_required
+from .models import Chat
 
-# Create your views here.
 
-def index (request):
-    return render(request,'Chat/index.html')
-
-def room(request,room_name):
+@login_required(login_url="login")
+def index(request):
+    user = request.user
+    
+    chat_rooms = Chat.objects.filter(members = user)
+    print(chat_rooms)
     context={
-        'room_name': room_name
+        
+        
+        
+        'chat_rooms' : chat_rooms
     }
-    return render(request,'Chat/room.html',context)
+    
+    
+    
+    return render(request, 'chat/index.html', context)
+
+@login_required(login_url="login")
+def room(request, room_name):
+    
+    user = request.user
+    
+    chat_model = Chat.objects.filter(roomname=room_name)
+    
+    
+    
+    if not chat_model.exists():
+        chat = Chat.objects.create(roomname=room_name)
+        chat.members.add(user)
+    else:
+        chat_model[0].members.add(user)
+    
+    
+    
+    username = request.user.username
+    
+    context = {
+
+        'room_name': room_name,
+        'username': mark_safe(json.dumps(username))
+    }
+
+    return render(request, "chat/room.html",context)
